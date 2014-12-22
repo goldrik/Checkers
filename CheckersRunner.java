1edab3fbf7eaa66f4fe272382021a548006e09b6
Aurik Sarker <auriksarker@gmail.com>
2014-12-22 01:27:44 -0800

    Java Files

---
+++ CheckersRunner.java
@@ -0,0 +1,11 @@
+package checkersPackage;
+
+import checkersPackage.Checkers;
+
+public class CheckersRunner {
+	public static void main(String[] args) throws InterruptedException
+	{
+		Checkers check = new Checkers();
+		check.newGame(); 
+	}
+}
